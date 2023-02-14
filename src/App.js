5d7316c751e5a80044d1f60ec733b87807ffe328
Jay (Jaehoon) Jung <113634739+JayJung98@users.noreply.github.com>
2023-02-13 21:25:58 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react';
 
+//JavaScript
 function App() {
+  
+  let [title, changeTitle] = useState(['First post', 'Second post', 'Third post']);
+  let [thumb, thumbUp] = useState(0);
+
+  //HTML 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <div className='black-nav'>
+            <div>Jay's Portfolio</div>
+        </div>
+        <div className='list'>
+          <h3> { title[0] } <span onClick={ () => { thumbUp(thumb + 1)} }>👍🏻</span> { thumb } </h3>
+          <p>February 11</p>
+          <hr/> 
+        </div>
+
+        <div className='list'>
+          <h3> { title[1] } </h3>
+          <p>February 12</p>
+          <hr/>
+        </div>
+
+        <div className='list'>
+          <h3> { title[2] } </h3>
+          <p>February 13</p>
+          <hr/>
+        </div>
+        
     </div>
   );
 }
 
 export default App;
+// useState: 
+// react: 
+// abstraction
+// full stack
